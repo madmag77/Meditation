618f7b6b1ca9cf563f278204828712eb24741fcd
Goncharov Artem <artemswork@mail.ru>
2017-06-25 14:27:13 +0700

    Compatibility with Xcode 8

--- Meditation/ViewController.swift
+++ Meditation/ViewController.swift
@@ -12,9 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var timePicker: UIPickerView!
     
-    var workoutSession: HKWorkoutSession?
+    //var workoutSession: HKWorkoutSession?
     var healthStore: HKHealthStore = HKHealthStore()
-    var workoutStartDate: Date! = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,18 +40,18 @@ class ViewController: UIViewController {
 
     @IBAction func tapOnMeditate(_ sender: Any) {
         // Create a new workout session
-        workoutStartDate = Date()
-        self.workoutSession = HKWorkoutSession(activityType: .running, locationType: .indoor)
-        self.workoutSession!.delegate = self as HKWorkoutSessionDelegate;
-        
-        // Start the workout session
-        self.healthStore.start(self.workoutSession!)
-        
-        // This is the type you want updates on. It can be any health kit type, including heart rate.
-        let heartRateType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)
-        
-        // Match samples with a start date after the workout start
-        let predicate = HKQuery.predicateForSamples(withStart: workoutStartDate, end: nil, options: [])
+//        workoutStartDate = Date()
+//        self.workoutSession = HKWorkoutSession(activityType: .running, locationType: .indoor)
+//        self.workoutSession!.delegate = self as HKWorkoutSessionDelegate;
+//        
+//        // Start the workout session
+//        self.healthStore.start(self.workoutSession!)
+//        
+//        // This is the type you want updates on. It can be any health kit type, including heart rate.
+//        let heartRateType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate)
+//        
+//        // Match samples with a start date after the workout start
+//        let predicate = HKQuery.predicateForSamples(withStart: workoutStartDate, end: nil, options: [])
        /*
         let query = HKAnchoredObjectQuery(type: heartRateType!,
                                           predicate: nil,
@@ -79,37 +78,37 @@ class ViewController: UIViewController {
                                             print("Done!")
                      */
         
-        let heartRateQuery = HKAnchoredObjectQuery(type: heartRateType!,
-                                                   predicate: predicate,
-                                                   anchor: HKQueryAnchor(fromValue: Int(HKAnchoredObjectQueryNoAnchor)),
-                                                   limit: 0) { [unowned self](query, newSamples, deletedSamples, newAnchor, error) -> Void in
-                                                    print("query:\n")
-                                                    for sample in newSamples! {
-                                                        print(sample)
-                                                    }
-        }
-        
-        // This is called each time a new value is entered into HealthKit (samples may be batched together for efficiency)
-        heartRateQuery.updateHandler = { (query, samples, deletedObjects, anchor, error) -> Void in
-            print("update:\n")
-            for sample in samples! {
-                print(sample)
-            }
-        }
-        
-        // Start the query
-        self.healthStore.execute(heartRateQuery)
+//        let heartRateQuery = HKAnchoredObjectQuery(type: heartRateType!,
+//                                                   predicate: predicate,
+//                                                   anchor: HKQueryAnchor(fromValue: Int(HKAnchoredObjectQueryNoAnchor)),
+//                                                   limit: 0) { [unowned self](query, newSamples, deletedSamples, newAnchor, error) -> Void in
+//                                                    print("query:\n")
+//                                                    for sample in newSamples! {
+//                                                        print(sample)
+//                                                    }
+//        }
+//        
+//        // This is called each time a new value is entered into HealthKit (samples may be batched together for efficiency)
+//        heartRateQuery.updateHandler = { (query, samples, deletedObjects, anchor, error) -> Void in
+//            print("update:\n")
+//            for sample in samples! {
+//                print(sample)
+//            }
+//        }
+//        
+//        // Start the query
+//        self.healthStore.execute(heartRateQuery)
     }
     
 }
 
-extension ViewController: HKWorkoutSessionDelegate{
-    public func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
-        print(toState)
-    }
-    
-    public func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
-        print(error)
-    }
-}
+//extension ViewController: HKWorkoutSessionDelegate{
+//    public func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+//        print(toState)
+//    }
+//    
+//    public func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+//        print(error)
+//    }
+//}
 
